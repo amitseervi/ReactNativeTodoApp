0e2a2545fe9cbb634ba8296673c1dcc5c5f634d0
Amit <amitchaudhary.adc@gmail.com>
2020-08-28 10:16:03 +0530

    Adding navigation

--- app/components/BottomBarContainer.tsx
+++ app/components/BottomBarContainer.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
-import {View} from 'react-native';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 import {
   ActiveTodoList,
   CompletedTodoList,
 } from '../containers/TodoListContainer';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {
+  createBottomTabNavigator,
+  BottomTabNavigationOptions,
+} from '@react-navigation/bottom-tabs';
+import {RouteProp} from '@react-navigation/native';
 
 type Tab = {
   key: string;
@@ -18,9 +21,40 @@ type Tab = {
 const Tab = createBottomTabNavigator();
 
 export default class BottomBarContainer extends React.Component {
+  private myScreenOptions = (routeName: string): BottomTabNavigationOptions => {
+    return {
+      title: 'Hello',
+      tabBarLabel: 'Test',
+      tabBarIcon: (props: {focused: boolean; color: string; size: number}) => {
+        switch (routeName) {
+          case 'complete': {
+            return (
+              <Ionicons
+                name="checkmark-done"
+                size={props.size}
+                color={props.color}
+              />
+            );
+          }
+          default: {
+            return (
+              <Ionicons name="list" size={props.size} color={props.color} />
+            );
+          }
+        }
+      },
+    };
+  };
   render() {
     return (
-      <Tab.Navigator>
+      <Tab.Navigator
+        lazy={true}
+        backBehavior="history"
+        screenOptions={(props: {
+          route: {
+            name: string;
+          };
+        }) => this.myScreenOptions(props.route.name)}>
         <Tab.Screen name="active" component={ActiveTodoList} />
         <Tab.Screen name="complete" component={CompletedTodoList} />
       </Tab.Navigator>
